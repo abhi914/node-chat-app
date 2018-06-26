0295d7e7457fd6493a2a19d63fb73b816b23f74a
abhi941 <abhigyan.nayak89@gmail.com>
2018-06-26 23:07:00 +0530

    added socket.IO

--- server/server.js
+++ server/server.js
@@ -1,19 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+let server = http.createServer(app);
+
+let io = socketIO(server);
+
+
+
 app.use(express.static(path.join(__dirname,'../public')));
 
 
-// app.get('/',(req,res) => {
+io.on('connection',(socket) => {
+    console.log('New User Connected');
 
-// });
+
+    socket.on('disconnect', () => {
+        console.log(`User disconnect`);
+    });
+});
 
 
-app.listen(port, (error) => {
+
+
+
+server.listen(port, (error) => {
     if(error) {
         return console.error(error);
     }
